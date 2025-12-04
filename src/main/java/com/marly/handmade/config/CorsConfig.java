2c1e5acd0440489cd7fb047c377715d6e2481a0c
Josue049 <josue09guti@gmail.com>
2025-12-04 02:21:52 -0500

    CORS corregido

--- src/main/java/com/marly/handmade/config/CorsConfig.java
+++ src/main/java/com/marly/handmade/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5174")
+                        .allowedOrigins("https://marly.azurewebsites.net/")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH")
                         .allowedHeaders("*")
                         .allowCredentials(false);
